2d5197c5f39e1adcce018f3b7f0b1d80af97b156
Priyansh Thakar <priyanshthakar@gmail.com>
2026-07-13 19:09:14 -0400

    fetch complete

--- js/json.js
+++ js/json.js
@@ -3,24 +3,32 @@ const header = document.querySelector("header");
 const section = document.querySelector("section");
 
 // STEP 3a: Create the asynchronous function populate()
+async function populate() {
+    // Introducing JavaScript Object Notation (JSON): https://json.org/
+    // STEP 4a: Create i-scream.json file with companyName, headOffice, established, active, topFlavors(name, calories, type, ingredients, image) */
+    // STEP 4b: Store the URL of a JSON file in a variable */
+    let url = "https://priyansht.github.io/26S-JavaScript-02-Week10/js/i-scream.json";
 
-// Introducing JavaScript Object Notation (JSON): https://json.org/
-// STEP 4a: Create i-scream.json file with companyName, headOffice, established, active, topFlavors(name, calories, type, ingredients, image) */
-// STEP 4b: Store the URL of a JSON file in a variable */
-
-// STEP 5: Use the new URL to create a new request object
-
-// STEP 6: Make a network request with the fetch() function, which returns a Response object
-
-// STEP 7: Capture the returned Response object and covert to a JSON object using json()
-
-// STEP 8: Output the iScream JSON object to the console
-
-// STEP 9a: Invoke the populateHeader function here, then build it below
-
-// STEP 10a: Invoke the showTopFlavors function here, then build it below
+    // STEP 5: Use the new URL to create a new request object
+    let request = new Request(url);
+    
+    // STEP 6: Make a network request with the fetch() function, which returns a Response object
+    let response = await fetch(request);
+    
+    // STEP 7: Capture the returned Response object and covert to a JSON object using json()
+    let responseJson = await response.json();
+    
+    // STEP 8: Output the iScream JSON object to the console
+    console.log(responseJson);
+    
+    // STEP 9a: Invoke the populateHeader function here, then build it below
+    
+    // STEP 10a: Invoke the showTopFlavors function here, then build it below
+    
+}
 
 // STEP 3b: Call the populate() function
+populate();
 
 /* STEP 9b: Build out the populateHeader() function */
 
